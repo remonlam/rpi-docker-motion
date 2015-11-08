a7349a3dd81ea151b774647f7bb029c162677f4f
Remon Lam <remon.lam@lamnet.nl>
2015-11-08 17:19:25 +0100

    fixed installer

--- install-motion.sh
+++ install-motion.sh
@@ -1,29 +1,23 @@
-# Update
-#apt-get update
+# Update image
+apt-get update
 
-# Install Motion
-apt-get install motion
-apt-get install libjpeg62
+# Install packages
+apt-get install -y wget tar nano motion libjpeg62
 
-# Remove old Motion config file (just to be sure)
+# Download sources
+wget -P /usr/lib/arm-linux-gnueabihf https://github.com/remonlam/rpi-docker-motion/raw/master/libs/libavcodec.so.53
+wget -P /usr/lib/arm-linux-gnueabihf https://github.com/remonlam/rpi-docker-motion/raw/master/libs/libavformat.so.53
+wget -P /usr/lib/arm-linux-gnueabihf https://github.com/remonlam/rpi-docker-motion/raw/master/libs/libavutil.so.51
+wget -P /usr/lib/arm-linux-gnueabihf https://github.com/remonlam/rpi-docker-motion/raw/master/libs/libdirac_encoder.so.0
+wget -P /usr/lib/arm-linux-gnueabihf https://github.com/remonlam/rpi-docker-motion/raw/master/libs/libgcrypt.so.11
+wget -P /usr/lib/arm-linux-gnueabihf https://github.com/remonlam/rpi-docker-motion/raw/master/libs/libgnutls.so.26
+wget -P /usr/lib/arm-linux-gnueabihf https://github.com/remonlam/rpi-docker-motion/raw/master/libs/libopenjpeg.so.2
+wget -P /usr/lib/arm-linux-gnueabihf https://github.com/remonlam/rpi-docker-motion/raw/master/libs/librtmp.so.0
+wget -P /usr/lib/arm-linux-gnueabihf https://github.com/remonlam/rpi-docker-motion/raw/master/libs/libtasn1.so.3
+wget -P /usr/lib/arm-linux-gnueabihf https://github.com/remonlam/rpi-docker-motion/raw/master/libs/libx264.so.123
+
+# Remove old motion config
 rm -rf /etc/motion/motion.conf
 
-# Download new Motion config file (note this config uses port 80)
+# Download motion config file
 wget -P /etc/motion https://raw.githubusercontent.com/remonlam/rpi-docker-motion/master/motion.conf
-
-# Download
-wget -P / https://raw.githubusercontent.com/remonlam/rpi-docker-motion/master/startmotion.sh
-
-# Make file executable
-chmod 755 /startmotion.sh
-
-# Create new directory
-mkdir -p /home/pi/mmal
-
-# Download motion-mmal
-wget -P /home/pi/mmal https://github.com/remonlam/rpi-docker-motion/raw/master/motion-mmal-opt.tar.gz
-
-# Untar motion-mmal
-tar -zxvf /home/pi/mmal/motion-mmal.tar.gz
-
-wget -P /home/pi/mmal https://raw.githubusercontent.com/remonlam/rpi-docker-motion/master/motion-mmalcam.conf
